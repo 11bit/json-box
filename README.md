9a4e99bbdca565f85ed598b241d117b46579dd10
Ivan Buryak <buryak.ivan@yandex.ru>
2023-02-07 15:05:21 +0500

    Add more sample files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Json Box
+
+This is a demo web app for [jota-minidb](https://github.com/11bit/jotai-minidb) library.
+
+Add your json files and experiment with the data. All files are stored locally in the IndexedDB
+
+> Sample JSON files for examples are taken from https://github.com/dariusk/corpora
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,9 @@
 import { MiniDb } from "jotai-minidb";
 import { atom } from "jotai/vanilla";
 
-import planets from "./planets.json";
+import planets from "./bootstrap-data/planets.json";
+import chemicalElements from "./bootstrap-data/elements.json";
+import beerCategories from "./bootstrap-data/beer_categories.json";
 
 // UI state
 export const ui = {
@@ -57,6 +59,14 @@ export const filesDb = new MiniDb<JsonFile>({
     [PLANETS_JSON]: {
       createdAt: Date.now(),
       content: JSON.stringify(planets),
+    },
+    "beer_categories.json": {
+      createdAt: Date.now(),
+      content: JSON.stringify(beerCategories),
+    },
+    "chemical_elements.json": {
+      createdAt: Date.now(),
+      content: JSON.stringify(chemicalElements),
     },
   },
 });
